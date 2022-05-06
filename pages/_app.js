3a2f1ea3189e22f44f94608eb2d97b06509596eb
jakubmanczak <jakub@manczak.net>
2022-05-06 18:52:27 +0200

    provisional favicon & website title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/gractwo.svg" />
+				<title>Gractwo</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
