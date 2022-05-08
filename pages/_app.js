7e49c1a75951ab07efb11039b7be1a66d92d2921
Stanislaw <62724833+BoberITman@users.noreply.github.com>
2022-05-08 22:59:03 +0200

    Add content grid hotfix

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
 				<link rel="shortcut icon" href="/gractwo.svg" />
 				<title>Gractwo</title>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Lexend:wght@600&display=swap"
 					rel="stylesheet"
